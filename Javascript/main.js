5541c35b83952d483a83401007b075cbc10e29d6
raissasoareslima <raissasoaresl@hotmail.com>
2022-12-04 14:18:50 -0300

    Nova página de registro
    Utilizei CSS, HTML e JS para criar uma nova página de registro, consumindo uma API de cep.

--- Javascript/main.js
+++ Javascript/main.js
@@ -18,3 +18,38 @@ function multiplicacao() {
     var s = n1 * n2 
     res.innerHTML = `O total de episódios da série é igual a ${s}`
     }
+
+//Consultando API de CEP(viacep.com.br) com Fetch API
+const cep = document.querySelector("#cep")
+
+const showData = (result)=>{
+    for(const campo in result){
+        if(document.querySelector("#"+campo)){
+            document.querySelector("#"+campo).value = result[campo]
+        }
+    }
+}
+//Propriedade (ação blur) adiciona um disparo de evento no campo cep para: sempre que tirar o mouse do input executar esse evento;
+// Variavel search para procurar e retirar o "-" do cep para enviar ao site viacep para execução correta
+cep.addEventListener("blur",(e)=>{
+
+    //condição para caso o campo cep esteja vazio ele irá limpar o restante dos outros campos relacionados
+    if(cep.value == "") {
+        document.querySelector("#logradouro").value = ""
+        document.querySelector("#bairro").value = ""
+        document.querySelector("#localidade").value = ""
+        document.querySelector("#uf").value = ""
+    }
+    let search = cep.value.replace("-","")
+    const options = {
+        method: 'GET',
+        mode: 'cors',
+        cache: 'default'
+    }
+
+    fetch(`https://viacep.com.br/ws/${search}/json/`, options)
+    .then(response =>{ response.json()
+        .then( data => showData(data))
+    })
+    .catch(e => console.log('Deu Erro: '+ e,message))
+})
